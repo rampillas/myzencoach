8f21727e806dcf148f67cf95189e2bea58f8c9b8
ceokin <cesar_ceo@hotmail.com>
2017-02-22 22:42:21 +0000

    Inicio de implementacion del Ranking: listView etc

--- app/src/main/java/com/unir/grupo2/myzeancoach/ui/MEssentialInfo/RankingFragment.java
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/MEssentialInfo/RankingFragment.java
@@ -3,11 +3,17 @@ package com.unir.grupo2.myzeancoach.ui.MEssentialInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.unir.grupo2.myzeancoach.R;
+import com.unir.grupo2.myzeancoach.ui.model.Score;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Cesar on 22/02/2017.
@@ -15,9 +21,26 @@ import com.unir.grupo2.myzeancoach.R;
 
 public class RankingFragment extends Fragment {
 
+
+    //List of score elements where we save our rankings
+    List<Score> rankingItemList;
+
+    //It will receive the taskItemList and it will be passed to the recyclerView
+   // RankingListAdapter rankingListAdapter;
+
+    //It will receive the adapter
+    RecyclerView recyclerView;
+
+     //LayoutManager reference
+    LinearLayoutManager linearLayoutManager;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rankingItemList = new ArrayList<>();
+
         return inflater.inflate(R.layout.ranking_layout,null);
     }
+
 }
---
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/model/Score.java
@@ -0,0 +1,28 @@
+package com.unir.grupo2.myzeancoach.ui.model;
+
+/**
+ * Created by Cesar on 22/02/2017.
+ */
+
+//Temporal class until the real model is ready
+public class Score {
+
+    private String name;
+    private String score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
