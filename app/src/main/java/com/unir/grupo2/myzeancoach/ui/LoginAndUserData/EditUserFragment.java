3c89f03a0b928610dc05c2d4c5ce17ba55aaed22
andres <rodriguezan@unican.es>
2017-02-27 21:48:39 +0100

    Create various UI Login Fragments

---
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/LoginAndUserData/EditUserFragment.java
@@ -0,0 +1,30 @@
+package com.unir.grupo2.myzeancoach.ui.LoginAndUserData;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.unir.grupo2.myzeancoach.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditUserFragment extends Fragment {
+
+
+    public EditUserFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_user, container, false);
+    }
+
+}
--- app/src/main/java/com/unir/grupo2/myzeancoach/ui/LoginAndUserData/RecoveryPasswordFragment.java
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/LoginAndUserData/RecoveryPasswordFragment.java
@@ -1,4 +1,4 @@
-package com.unir.grupo2.myzeancoach.ui.Login;
+package com.unir.grupo2.myzeancoach.ui.LoginAndUserData;
 
 import android.content.Context;
 import android.net.Uri;
@@ -10,8 +10,15 @@ import android.view.ViewGroup;
 
 import com.unir.grupo2.myzeancoach.R;
 
-
-public class LoginFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RecoveryPasswordFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RecoveryPasswordFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecoveryPasswordFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -23,13 +30,21 @@ public class LoginFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public LoginFragment() {
+    public RecoveryPasswordFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RecoveryPasswordFragment.
+     */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static RecoveryPasswordFragment newInstance(String param1, String param2) {
+        RecoveryPasswordFragment fragment = new RecoveryPasswordFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -50,10 +65,10 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_recovery_password, container, false);
     }
 
-
+    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -77,7 +92,16 @@ public class LoginFragment extends Fragment {
         mListener = null;
     }
 
-
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
