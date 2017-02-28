bc39dd309e25b9742bc6fa8d4491214b801f8f13
andres <rodriguezan@unican.es>
2017-02-28 18:15:10 +0100

    uPDATE Java Classes for Login

---
+++ app/src/main/java/com/unir/grupo2/myzeancoach/domain/LoginAndUserData/RecoveryPasswordServer.java
@@ -0,0 +1,8 @@
+package com.unir.grupo2.myzeancoach.domain.LoginAndUserData;
+
+/**
+ * Created by andres on 28/02/2017.
+ */
+
+public class RecoveryPasswordServer {
+}
--- app/src/main/java/com/unir/grupo2/myzeancoach/ui/LoginAndUserData/CreateUserFragment.java
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/LoginAndUserData/CreateUserFragment.java
@@ -1,109 +1,99 @@
 package com.unir.grupo2.myzeancoach.ui.LoginAndUserData;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
+import com.mukesh.countrypicker.fragments.CountryPicker;
+import com.mukesh.countrypicker.interfaces.CountryPickerListener;
 import com.unir.grupo2.myzeancoach.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CreateUserFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CreateUserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.OnClick;
+import butterknife.OnItemSelected;
+
+
 public class CreateUserFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String Pais = "";
+    int EstudiosPersona = 0;
+    @BindView(R.id.UsuarioSingUp)
+    EditText Usuario;
+    @BindView(R.id.Password)
+    EditText Password;
+    @BindView(R.id.email)
+    EditText Email;
+    @BindView(R.id.UNombreSingUp)
+    EditText Nombre;
+    @BindView(R.id.datePickerText)
+    EditText Nacimiento;
+    @BindView(R.id.radio_man)
+    RadioButton Hombre;
+    @BindView(R.id.radio_woman)
+    RadioButton Mujer;
+    @BindView(R.id.PaisList)
+    Button ListaPaises;
+    @BindView(R.id.ciudad)
+    EditText Ciudad;
+    @BindView(R.id.radio_rural)
+    RadioButton Rural;
+    @BindView(R.id.radio_urbana)
+    RadioButton Urbana;
+    @BindView(R.id.radio_si)
+    RadioButton Si;
+    @BindView(R.id.radio_no)
+    RadioButton No;
+    @BindView(R.id.StudyList)
+    Spinner Estudios;
+    @BindView(R.id.okButton)
+    Button okButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @OnClick(R.id.okButton)
+    public void crearCuenta() {
+        // TODO comprobar que loscampos estan rellenos
+        // TODO hacer la creacion de usuario
+        // TODO volver al login Fragment
 
-    private OnFragmentInteractionListener mListener;
-
-    public CreateUserFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreateUserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateUserFragment newInstance(String param1, String param2) {
-        CreateUserFragment fragment = new CreateUserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @OnClick(R.id.PaisList)
+    public void ListaPaises() {
+        CountryPicker picker = CountryPicker.newInstance("Select Country");
+        picker.show(getFragmentManager(), "COUNTRY_PICKER");
+        picker.setListener(new CountryPickerListener() {
+            @Override
+            public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID) {
+                Pais = name;
+            }
+        });
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    @OnItemSelected(R.id.StudyList)
+    public void spinnerItemSelected(Spinner spinner, int position) {
+        EstudiosPersona = spinner.getSelectedItemPosition();
     }
 
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.SIGNUP_STUDY_LEVELS, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        Estudios.setAdapter(adapter);
         return inflater.inflate(R.layout.create_user, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
