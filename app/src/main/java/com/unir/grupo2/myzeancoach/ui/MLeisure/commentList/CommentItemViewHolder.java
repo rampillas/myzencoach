9f1e2d4d2c7050c46b579e056a8301a548da4a3a
ceokin <cesar_ceo@hotmail.com>
2017-03-13 00:05:44 +0000

    Public homepage interface -> amost completed-> versin 4 add comments

---
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/MLeisure/commentList/CommentItemViewHolder.java
@@ -0,0 +1,30 @@
+package com.unir.grupo2.myzeancoach.ui.MLeisure.commentList;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.unir.grupo2.myzeancoach.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Cesar on 12/03/2017.
+ */
+
+public class CommentItemViewHolder extends RecyclerView.ViewHolder{
+
+    @BindView(R.id.date_comment_textView) TextView dateTextView;
+    @BindView(R.id.description_comment_textView) TextView descriptionTextView;
+
+    public CommentItemViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this,itemView);
+    }
+
+    public void bind(final CommentItem commentItem) {
+
+
+    }
+}
---
+++ app/src/main/java/com/unir/grupo2/myzeancoach/ui/MLeisure/commentList/CommentListAdapter.java
@@ -0,0 +1,8 @@
+package com.unir.grupo2.myzeancoach.ui.MLeisure.commentList;
+
+/**
+ * Created by Cesar on 12/03/2017.
+ */
+
+public class CommentListAdapter {
+}
